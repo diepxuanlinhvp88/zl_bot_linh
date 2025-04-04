e01e08606b8d6e55c8b9413a0abc88a67a12b7a8
diepxuanlinhvp88 <diepxuanlinhvp88@gmail.com>
2025-04-04 15:18:09 +0700

    change cookies

--- bot/config.py
+++ bot/config.py
@@ -8,18 +8,21 @@ db_config = {
 imei = "38e75fc0-cb98-4857-9cd4-bd0ebe6b592e-16453d6e2683b8800ded2a27c7f595d9"
 cookies = {
     "googtrans": "/auto/vi",
-    "_ga": "GA1.2.1261768382.1719684775",
-    "_ga_907M127EPP": "GS1.1.1738998920.5.1.1738999996.60.0.0",
     "_ga_RYD7END4JE": "GS1.2.1739340322.4.1.1739340322.60.0.0",
-    "_gid": "GA1.2.630579600.1740466727",
-    "_zlang": "vn",
-    "zpsid": "pmv4.432189662.2.zpmvynqTG5V3hO9E4Hr0LsXkCcCkBt5l92jsPMmB57O9Z63k79OgwdeTG5S",
-    "zpw_sek": "Li5D.432189662.a0.DEImZVmreMCYXMDwtJMXOuSNn0ZU3uCgZbVf2PjbmmAJ1vSlcmUJMxvnwIsB29n1WK71Jc8XC4D5D2e9GLAXOm",
+    "_gcl_au": "1.1.1732737999.1740721972",
+    "_fbp": "fb.1.1740721972448.834961435955504970",
+    "_ga_907M127EPP": "GS1.1.1740721277.6.1.1740722041.60.0.0",
+    "_ga_E63JS7SPBL": "GS1.1.1740721971.1.0.1740722418.60.0.0",
+    "_ga": "GA1.2.1261768382.1719684775",
+    "zpsid": "uey9.432189662.3.UMQAwRxwux1WIY6filhZiCk9aOIDoDA8XypLWfIBUMPY4gAFlqQzLjdwux0",
+    "zpw_sek": "_Ayc.432189662.a0.Ux3kZhF6xf1jTYo9aiRkaCZaY_kH_Cp0tgISuSA3ZyY1_fkczeI-_Og_dyV4-zEopkoV2cKQ1j6CnrFELQ7ka0",
     "__zi": "3000.SSZzejyD3yaynEwypKq6mI3IuhFS6mJ09Twj_i5L2jXcWh_qZnaEsodFi_wB7WIUBiwmkumU0PueohMtD0.1",
+    "zoaw_sek": "BsTP.783721872.2.KqvPqXTUIXI7-_mw5rvA51TUIXGNNaeK5YsH-d1UIXG",
+    "zoaw_type": "0",
+    "_zlang": "vn",
     "__zi-legacy": "3000.SSZzejyD3yaynEwypKq6mI3IuhFS6mJ09Twj_i5L2jXcWh_qZnaEsodFi_wB7WIUBiwmkumU0PueohMtD0.1",
     "app.event.zalo.me": "7086971815617337040",
 }
-
 
 
 mapbox_access_token = "pk.eyJ1IjoiZGllcHh1YW5saW5oIiwiYSI6ImNtNzlicWFyMTAzcjUya3B6emlndnl5aWYifQ.aXGKJJvspqirGYZn0op3Uw"
