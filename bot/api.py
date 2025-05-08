25dfd733ce49b3c4ce90505445612469a177b9f5
diepxuanlinhvp88 <124590159+diepxuanlinhvp88@users.noreply.github.com>
2025-05-08 14:27:49 +0000

    Update api.py

--- bot/api.py
+++ bot/api.py
@@ -114,7 +114,7 @@ def get_db():
         
 def geocoding_openmaps(address):
     base_url = "https://mapapis.openmap.vn/v1"
-    apikey = "tKqXn5rUkQ5QOvZrMdrXa5g279xpOxt7"
+    apikey = "tKqXn5rUkQ55g279xpOxt7"
 
     res = requests.get(f"{base_url}/geocode/forward?address={address}&apikey={apikey}")
 
